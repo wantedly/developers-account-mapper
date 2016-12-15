cdc0fe3a8ff8176a57c40d4c524fbf602d886cfb
Shimpei Otsubo <potsbo@gmail.com>
2016-12-15 13:18:36 +0900

    Implement register

--- command/register.go
+++ command/register.go
@@ -2,6 +2,11 @@ package command
 
 import (
 	"strings"
+	"os"
+	"log"
+	"fmt"
+	"github.com/wantedly/github-username-converter/store"
+	"github.com/wantedly/github-username-converter/models"
 )
 
 type RegisterCommand struct {
@@ -9,7 +14,27 @@ type RegisterCommand struct {
 }
 
 func (c *RegisterCommand) Run(args []string) int {
-	// Write your code here
+	var loginName, githubUsername string
+	if len(args) == 1 {
+		loginName = os.Getenv("USER")
+		githubUsername = args[0]
+	} else if len(args) == 2 {
+		loginName = args[0]
+		githubUsername = args[1]
+	} else {
+		log.Println(c.Help())
+		return 1
+	}
+
+	s := store.NewDynamoDB()
+
+	user := models.NewUser(loginName, githubUsername)
+	err := s.AddUser(user)
+	if err != nil {
+		log.Println(err)
+		return 1
+	}
+	fmt.Printf("user %v added.\n", user)
 
 	return 0
 }
