ef046330076c860ac5edf491c03a8c3a15078546
Shimpei Otsubo <potsbo@gmail.com>
2016-12-21 16:31:56 +0900

    Fix DynamoDB attribute data type

--- store/dynamo_db.go
+++ store/dynamo_db.go
@@ -34,7 +34,7 @@ func (d *DynamoDB) ListUsers() ([]*models.User, error) {
 	var users []*models.User
 
 	for _, item := range resp.Items {
-		users = append(users, models.NewUser(*item["LoginName"].S, *item["GitHubUsername"].S, *item["SlackUsername"].S, *item["SlackUserId"]))
+		users = append(users, models.NewUser(*item["LoginName"].S, *item["GitHubUsername"].S, *item["SlackUsername"].S, *item["SlackUserId"].S))
 	}
 
 	return users, nil
