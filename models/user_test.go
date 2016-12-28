4b29a4f3f1733ff7dd008eb3216b043a03d01d27
Shimpei Otsubo <potsbo@gmail.com>
2016-12-28 18:06:02 +0900

    Refactor Add SetUser

--- models/user_test.go
+++ models/user_test.go
@@ -4,13 +4,17 @@ import (
 	"testing"
 )
 
-func TestSlackMention(t *testing.T) {
-	user := &User {
+func SetUser() *User {
+	return &User {
 		LoginName: "loginName",
 		GitHubUsername: "github_user",
 		SlackUsername: "slack_user",
 		SlackUserId: "SLACKID",
 	}
+}
+
+func TestSlackMention(t *testing.T) {
+	user := SetUser()
 
 	expect := "<@SLACKID|slack_user>"
 	actual := user.SlackMention()
