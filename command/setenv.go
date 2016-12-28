7cf55d8eb5998d033e0fdfe73d1baba61e2d5fbb
Shimpei Otsubo <potsbo@gmail.com>
2016-12-28 15:35:51 +0900

    Add setenv.go and besic functionality

---
+++ command/setenv.go
@@ -0,0 +1,26 @@
+package command
+
+import (
+	"log"
+	"github.com/wantedly/developers-account-mapper/store"
+	"os"
+)
+
+type SetenvCommand struct {
+	Meta
+}
+
+func (c *SetenvCommand) Run(args []string) int {
+	loginName := args[0]
+	if args[1] != "exec" {
+		log.Println("$ developers-account-mapper setenv <user> exec")
+		return 1
+	}
+
+	s := store.NewDynamoDB()
+
+	user, err := s.GetUserByLoginName(loginName)
+	os.Setenv("GITHUB_USERNAME", user.GitHubUsername)
+
+	return 0
+}
