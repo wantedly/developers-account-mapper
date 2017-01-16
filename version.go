76e281f857e6d538f80f95207ce3abc94502fb23
Shimpei Otsubo <potsbo@gmail.com>
2017-01-16 23:44:17 +0900

    User var

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
-const Name string = "developers-account-mapper"
-const Version string = "0.1.0"
+var Name string = "developers-account-mapper"
+var Version string = "0.1.0"
 
 // GitCommit describes latest commit hash.
 // This value is extracted by git command when building.
