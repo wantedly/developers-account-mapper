ef555d9366e4b7c0fe3ff9307ff3def11e53dff9
Shimpei Otsubo <potsbo@gmail.com>
2017-01-16 23:55:36 +0900

    Remove comments

--- version.go
+++ version.go
@@ -2,8 +2,4 @@ package main
 
 var Name string = "developers-account-mapper"
 var Version string
-
-// GitCommit describes latest commit hash.
-// This value is extracted by git command when building.
-// To set this from outside, use go build -ldflags "-X main.GitCommit \"$(COMMIT)\""
 var GitCommit string
