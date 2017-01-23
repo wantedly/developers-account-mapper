89bbf63a8df6735711e52929146da7e8a718ee42
Shimpei Otsubo <potsbo@gmail.com>
2017-01-23 11:12:20 +0900

    Add to-slack-mention command

---
+++ command/to-slack-mention.go
@@ -0,0 +1,48 @@
+package command
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/wantedly/developers-account-mapper/store"
+)
+
+type ToSlackMention struct {
+	Meta
+}
+
+func (c *ToSlackMention) Run(args []string) int {
+	var loginName string
+	if len(args) == 0 {
+		loginName = os.Getenv("USER")
+	} else if len(args) == 1 {
+		loginName = args[0]
+	} else {
+		log.Println(c.Help())
+		return 1
+	}
+
+	s := store.NewDynamoDB()
+
+	user, err := s.GetUserByLoginName(loginName)
+	if err != nil {
+		log.Println(err)
+		return 1
+	}
+	fmt.Printf("SlackMention account for %s is: %s\n", loginName, user.SlackMention())
+
+	return 0
+}
+
+func (c *ToSlackMention) Synopsis() string {
+	return "Get <slack_mention> from <login_name>"
+}
+
+func (c *ToSlackMention) Help() string {
+	helpText := `
+
+`
+	return strings.TrimSpace(helpText)
+}
